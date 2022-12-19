011a6c33182831a9d43c5dc35ae787e1b3dd9454
Liujih2021 <liujihong2021@126.com>
2022-12-19 17:23:28 +0800

    Bacmethy

--- script/bacmethy.sh
+++ script/bacmethy.sh
@@ -6,9 +6,9 @@
 # Time-stamp: <2021-06-27 16:49:42 LiuJihong>
 usage() {
     echo "
-BTPMI V1.0.1 01/01/2022
+Bacmethy V1.0.1 01/01/2022
 <liujihong2021@126.com>
-usage:  btpmi.sh  -m <motifs.gff> -s <motifs.csv> -g <genome.fa> -p <prefix> -t <m6A, m4C or m5C>  [options] -T  -d -b -a -r 
+usage:  bacmethy.sh  -m <motifs.gff> -s <motifs.csv> -g <genome.fa> -p <prefix> -t <m6A, m4C or m5C>  [options] -T  -d -i -c -b -a -r 
 requirement: locally installed PROKKA,  bedtools and MEME  softwares
 
 
@@ -21,6 +21,7 @@ options
     -T      FOLDER  a floder contains TFs files in meme format (required; users can get from Calcute_PPM_console_final.py)
     -d      FLOAT   a undermethylation thresholds of fraction (default: 0.75)
     -i      INT     a identificationQV thresholds (default: 40)
+    -c      INT     a coverage thresholds (default: 30)
     -b      INT     number of bps before TSS (default: 500)
     -a      INT     number of bps after TSS (default: 100)
     -r      FILE    FASTA or GBK file to use as 1st priority (default '')
